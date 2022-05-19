a5a067c9c728d861adb96c162f80e1bc371f34ce
PatrySNCHZ <patrysanchez392@gmail.com>
2022-05-19 17:47:34 +0200

    cambio nombres y añadidos htmls

---
+++ src/main/resources/import.sql
@@ -0,0 +1,2 @@
+
+INSERT INTO mascotas (id_mascota, id_propietario, Foto, Nombre, Especie, Edad, Raza, Descripcion, Chip, Genero) VALUES(1, 2, "https://pbs.twimg.com/media/FS5PnB7WYAEfEos?format=jpg&name=large","Vane", "perro", 3, "chucho","enano",724111111111111, "Femenino" );
